a851f09fc6c706ec1f5f2d2e3023add44a7e6838
Yassine BAJDA <bajdamr@gmail.com>
2023-08-10 14:41:53 -0700

    C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,9 @@
 /**
  *main - Entry point
  *Description: print a quute usning write function
- *Return: 1 (error)
+ *Return: 1
 */
 int main(void)
-{	char quo[]("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	write(1 .quo. 59);
+{	fwrite(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
