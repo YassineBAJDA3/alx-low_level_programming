8f186d9dc1aa5d14b1db2873df593b2e78f8b9ec
Yassine BAJDA <bajdamr@gmail.com>
2023-08-10 08:22:31 -0700

    yassine

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,14 @@
 #include<stdoi.h>
-int mian(void){
-  puts("\"Programming is like building a multilingual puzzle");	
-  return(0);
+/**
+ * main - Entry point 
+ * 
+ * descripoin: A C program that prints wit put funcyion 
+ *
+ * return: alawys 0 (success
+ */
+
+int mian(void)
+{
+puts("\"Programming is like building a multilingual puzzle");	
+return (0);
 }
