6c1da5b62615e2b11d4babc21d12692489926f29
Yassine BAJDA <bajdamr@gmail.com>
2023-08-12 05:45:25 -0700

    possible different combinations of three digits.

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Enntry point
+ *
+ * Descriptoint:  prints all possible different combinations of three digits.
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j; j = 0; i < 10; i++)
+		{
+			for (int k; k = 0; k++)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
+				putchar('\n');
+				putchar(' ');
+				putchar(',');
+			}
+		}
+	}
+	return (0);
+}
