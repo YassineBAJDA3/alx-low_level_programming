35ac81ad1a1ef89b0b7f5abb6b18d5fe418ca20c
Yassine BAJDA <bajdamr@gmail.com>
2023-08-17 15:42:19 -0700

    function that multiplies two integers.

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include"main.h"
+#include <stdio.h>
+
+/**
+ * mul - multiplies two integers.
+ *
+ * @a: the first int
+ * @b: the second int
+ * Return: Always 0.
+*/
+
+int mul(int a, int b)
+{
+	int mul = a * b;
+
+	return (mul);
+}
+
