344d7bf55ec17a02f86b257c8a59b63abad7aeba
Yassine BAJDA <bajdamr@gmail.com>
2023-08-31 15:23:00 -0700

    function that returns the natural square root of a number.

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -14,3 +14,22 @@ int _sqrt_recursion(int n)
 
 	return (sqrt_recursive(n, 0)); /* Start recursion with a guess of 0 */
 }
+
+/**
+ * sqrt_recursive - Helper function for recursive calculation of square root
+ * @n: The number for which square root is to be calculated
+ * @guess: The current guess for the square root
+ *
+ * Return: The natural square root of the number, or -1 if not found
+ */
+int sqrt_recursive(int n, int guess)
+{
+	if (guess * guess == n)
+
+		return (guess; /* Found the exact square root */
+
+	if (guess * guess > n)
+		return (-1); /* No natural square root for n */
+
+	return (sqrt_recursive(n, guess + 1)); /* Increment the guess and recurse */
+}
