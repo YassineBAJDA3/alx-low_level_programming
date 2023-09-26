9bb7a6aeba822a45d12b704b5cf70e601a2e2e6e
Yassine BAJDA <bajdamr@gmail.com>
2023-09-26 06:10:50 -0700

    function that frees a listint_t list.

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,39 +1,41 @@
 #include "lists.h"
 
 /**
- * free_listint_safe - Frees a linked list of integers safely.
- * @h: A pointer to the pointer to the head of the list.
+ * free_listint_safe - frees a linked list
+ * @h: pointer to the first node in the linked list
  *
- * Return: The number of elements in the freed list.
+ * Return: number of elements in the freed list
  */
 size_t free_listint_safe(listint_t **h)
 {
 	size_t len = 0;
-	listint_t *temp, *current;
+	int diff;
+	listint_t *temp;
 
-	if (h == NULL || *h == NULL)
+	if (!h || !*h)
 		return (0);
 
-	current = *h;
-	while (current != NULL)
+	while (*h)
 	{
-		temp = current->next;
-
-		/* Check if the address difference is positive */
-		if (current < temp)
+		diff = *h - (*h)->next;
+		if (diff > 0)
 		{
-			free(current);
+			temp = (*h)->next;
+			free(*h);
+			*h = temp;
 			len++;
 		}
 		else
 		{
-			free(current);
-			break; /* Exit loop when loop detected */
+			free(*h);
+			*h = NULL;
+			len++;
+			break;
 		}
-
-		current = temp;
 	}
 
-	*h = NULL; /* Set the head to NULL after freeing all nodes */
+	*h = NULL;
+
 	return (len);
 }
+
